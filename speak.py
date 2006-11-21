e9392aa022c97515273af306dea726c68b99e04a
drewp <none>
2006-11-21 14:47:19 -0800

    more asound notes
    more asound notes
    now speaks argv[1]
    
    darcs-hash:20061121224719-ecde5-2b9b0f00591d749b9fb0e741e8e4ff285760a584.gz

--- speak.py
+++ speak.py
@@ -1,10 +1,51 @@
-import os, tempfile, socket
+#!/usr/bin/python
+
+import sys, os, tempfile, socket
 
 """
 ubuntu pkgs: festival festvox-rablpc16k (at least)
+
+alsa plugin specs:
+http://www.alsa-project.org/alsa-doc/alsa-lib/pcm_plugins.html
+http://alsa.opensrc.org/index.php?page=.asoundrc
+http://www.alsa-project.org/alsa-doc/doc-php/asoundrc.php
+
+use mpg123 like this:
+mpg123 -o alsa09 -a bathroom song.mp3
+
+volumes are set at master=100, pcm=75
+
+.asoundrc follows:
+pcm.dshare {
+    type dmix
+    ipc_key 2048
+    slave {
+        pcm "hw:0"
+        channels 2
+    }
+}
+pcm.bathroom {
+    type plug
+    slave {
+        pcm "dshare"
+    }
+    ttable.0.0 1
+    ttable.1.0 1
+}
+pcm.frontdoor {
+    type plug
+    slave {
+        pcm "dshare"
+    }
+    ttable.0.1 1
+    ttable.1.1 1
+}
+
+
 """
 
-def say(sableXmlText):
+def say(sableXmlText, where="frontdoor"):
+    """where is an alsa device name; see .asoundrc"""
     tf = tempfile.NamedTemporaryFile(suffix=".sable")
     tf.write("""<?xml version="1.0"?>
 <!DOCTYPE SABLE PUBLIC "-//SABLE//DTD SABLE speech mark up//EN" 
@@ -14,14 +55,15 @@ def say(sableXmlText):
     tf.flush()
     wav = tempfile.NamedTemporaryFile(suffix=".wav")
     text2wave = "text2wave"
-    if socket.gethostname() == "dash":
+    if socket.gethostname() == "xxdash":
         text2wave = "/my/dl/dl/festival/bin/text2wave"
     os.system("%s %s > %s" % (text2wave, tf.name, wav.name))
-    os.system("play %s" % wav.name)
+    os.system("aplay -D %s %s" % (where, wav.name))
     
 
 if __name__ == '__main__':
-    say("""
+    say(sys.argv[1])
+    """
 Without his penguin, <PITCH BASE="-20%"> which he left at home, </PITCH> he
        could not enter the restaurant.
-       """)
+       """
