330dbfacffd291b630520d4fd6113133d85c8f05
drewp <none>
2007-12-01 19:00:27 -0800

    add LinuxParallel (which just uses pyparallel)
    add LinuxParallel (which just uses pyparallel)
    also includes some failing attempts at controlling the usb parallel cable
    
    darcs-hash:20071202030027-ecde5-71a50544d09df35abdaed7fa5b9afa2026b7f574.gz

--- solarisparport.py
+++ solarisparport.py
@@ -1,6 +1,5 @@
 from fcntl import ioctl
 import os, struct
-import hubclient
 
 IOPWRITE = 2
 class SolarisParport(object):
@@ -32,3 +31,60 @@ class SolarisParport(object):
 
 # see also http://www.captain.at/programming/solaris/ for the structs,
 # IOP constants
+
+
+# not working yet
+class Usbprn(object):
+    """a solaris parallel port on usb"""
+    def __init__(self, file="/dev/printers/0"):
+        self.file = file
+
+    def open(self):
+        print "opening - sun hangs here"
+        self.fd = os.open(self.file, os.O_RDWR)
+        print "opened"
+        return "capabilities return value not implemented"
+
+    def write_data(self, value):
+        print "writing"
+        os.write(self.fd, chr(value))
+        print "wrote"
+
+class LinuxUsbParallel(object):
+    def __init__(self, file="/dev/usblp1"):
+        self.file = file
+
+    def open(self):
+         self.fd = os.open(self.file, os.O_RDWR)
+
+    def write_data(self, value):
+        print "writing"
+        os.write(self.fd, chr(value))
+        print "wrote"
+
+class LinuxParallel(object):
+    def __init__(self, port=0):
+        self.portNum = port
+
+    def open(self):
+        import parallel
+        self.port = parallel.Parallel(port=self.portNum)
+        self.device = self.port.device
+
+    def write_data(self, value):
+        self.port.setData(value)
+        
+        
+
+if __name__ == '__main__':
+    p = LinuxUsbParallel()
+    p.open()
+    import time
+    while 1:
+        print "hi"
+        p.write_data(255)
+        time.sleep(1)
+        print "lo"
+        p.write_data(0)
+        time.sleep(1)
+        
