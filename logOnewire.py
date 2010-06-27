7d9c6f7ae65b956f915c73d32d5d1ab43479b28d
drewp <drewp@bigasterisk.com>
2010-06-27 01:47:45 -0700

    new graphite logger for onewire devices on arduinos
    Ignore-this: a0dd71272189250f1ab3daf069fae2cc
    
    darcs-hash:20100627084745-312f9-b0ee78b554e691f71718e5d507aa1559a289be83.gz

---
+++ logOnewire.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from carbondata import CarbonClient
+import restkit, jsonlib, logging, time
+from logsetup import commonlogsetup
+
+log = commonlogsetup(filename=None)
+log.setLevel(logging.INFO)
+
+carbon = CarbonClient(serverHost='bang')
+
+while True:
+    now = time.time()
+    nextRead = now + 120
+    try:
+        shiftweb = restkit.Resource("http://plus:9014/")
+        temp = jsonlib.load(shiftweb.get("temperature").body)['temp']
+        carbon.send('system.house.temp.ariroom', temp, now)
+    except restkit.RequestError, e:
+        log.warn(e)
+    time.sleep(max(0, time.time() - nextRead))
