eff0469179e9e9596bbd7890fe5e4de592887a6e
drewp <drewp@bigasterisk.com>
2011-01-08 23:57:03 -0800

    clients for carbon/graphite
    Ignore-this: ab9bf872fde19c4036c5eb9f08f676e4
    
    darcs-hash:20110109075703-312f9-e803c2c6788ca40c81a517ed0cecf584400b91cb.gz

---
+++ carbondata.py
@@ -0,0 +1,36 @@
+"""
+upload data to a carbon server (http://graphite.wikidot.com/carbon) from twisted
+"""
+from __future__ import division
+import time, math
+from twisted.internet.protocol import Protocol, ReconnectingClientFactory
+from twisted.internet import reactor, task
+
+class CarbonFactory(ReconnectingClientFactory):
+    protocol = Protocol
+
+class CarbonClient(object):
+    def __init__(self, serverHost="localhost", port=2003):
+        self.carbonFactory = CarbonFactory()
+
+        self.conn = reactor.connectTCP(serverHost, port, self.carbonFactory)
+        
+    def send(self, metricPath, value, timestamp="now"):
+        # quietly fails if the connection isn't ready yet :(
+        if timestamp == "now":
+            timestamp = time.time()
+        # carbon quietly ignores floating-point times
+        self.conn.transport.write("%s %f %d\n" % (str(metricPath), # no unicode
+                                                  value, timestamp))
+
+if __name__ == '__main__':
+    c = CarbonClient()
+
+    def writeSin():
+        now = time.time()
+        print "sin", now
+        c.send('system.demo.sensor2.sintwisted3', 50 + 50 * math.sin(now))
+
+    task.LoopingCall(writeSin).start(2)
+
+    reactor.run()
