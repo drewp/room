48285fd34e4d8061a36a754e411f5bdf643277bd
drewp <drewp@bigasterisk.com>
2011-01-09 00:00:06 -0800

    logonewire host update, some err handling
    Ignore-this: c82bc0f55a1a93b9366f4f1f9abc7964
    
    darcs-hash:20110109080006-312f9-402069844a062c1ed70eab949e62c7dfc36b7e8c.gz

--- logOnewire.py
+++ logOnewire.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 from twisted.internet.task import LoopingCall
 from twisted.internet import reactor
-import restkit, jsonlib, logging, time
+import restkit, jsonlib, logging, time, socket
 from logsetup import commonlogsetup
 from carbondata import CarbonClient
 
 log = commonlogsetup(filename=None)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARN)
 
 carbon = CarbonClient(serverHost='bang')
 
 def update():
     now = time.time()
     try:
-        shiftweb = restkit.Resource("http://plus:9014/", timeout=3)
-        temp = jsonlib.read(shiftweb.get("temperature").body, use_float=True)['temp']
+        shiftweb = restkit.Resource("http://star:9014/", timeout=5)
+        temp = jsonlib.read(shiftweb.get("temperature").body_string(), use_float=True)['temp']
         log.info("write temp %r" % temp)
         carbon.send('system.house.temp.ariroom', temp, now)
         log.debug("wrote to carbon")
-    except restkit.RequestError, e:
+    except (restkit.RequestError, socket.error), e:
         log.warn(e)
 update()
 LoopingCall(update).start(interval=120)
