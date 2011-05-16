6afde79ab8e31df1b3fcab45b0d03b41665e2202
drewp <drewp@bigasterisk.com>
2011-05-15 23:29:19 -0700

    logOnewire.py grabs front door temperature too, writes to carbon
    Ignore-this: b672ab8bd7d8cde3ba31888be7bcde04
    
    darcs-hash:20110516062919-312f9-fa4cd08a4e6e861c94a65de9a59d16d8d01449b0.gz

--- logOnewire.py
+++ logOnewire.py
@@ -12,14 +12,20 @@ carbon = CarbonClient(serverHost='bang')
 
 def update():
     now = time.time()
-    try:
-        shiftweb = restkit.Resource("http://star:9014/", timeout=5)
-        temp = jsonlib.read(shiftweb.get("temperature").body_string(), use_float=True)['temp']
-        log.info("write temp %r" % temp)
-        carbon.send('system.house.temp.ariroom', temp, now)
-        log.debug("wrote to carbon")
-    except (restkit.RequestError, socket.error), e:
-        log.warn(e)
-update()
+    for url, name in [("http://star:9014/", "ariroom"),
+                      ("http://space:9080/", "frontDoor"),
+                      ]:
+        for tries in range(3):
+            try:
+                shiftweb = restkit.Resource(url, timeout=5)
+                temp = jsonlib.read(shiftweb.get("temperature").body_string(), 
+                                    use_float=True)['temp']
+                log.info("write %s temp %r", name, temp)
+                carbon.send('system.house.temp.%s' % name, temp, now)
+                log.debug("wrote to carbon")
+                break
+            except Exception, e:
+                log.warn(e)
+
 LoopingCall(update).start(interval=120)
 reactor.run()
